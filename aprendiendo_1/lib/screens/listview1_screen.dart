f7682e3312eb62596e80abd3d13ed7c9ac0819e0
Gabriel ASM <g.aillapan01@ufromail.cl>
2022-11-10 23:19:31 -0300

    recorrer una lista y mostrarla con widgets

--- aprendiendo_1/lib/screens/listview1_screen.dart
+++ aprendiendo_1/lib/screens/listview1_screen.dart
@@ -4,15 +4,27 @@ class ListView1Screen extends StatelessWidget {
   //siempre es buena practica colocar el key
   //nos permite controlar el widget
   const ListView1Screen({Key? key}) : super(key: key);
+
+  final options = const  ["Megaman" , "Metal gear", "Super Smash" , "Final fANTASY"]; 
   
+
+  // Aveces el hotreload falla, hacer hot restart si es que asi 
+
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
         title: const Text("Listview Tipo 1"),
       ),
-      body: ListView(children: const [
-        Text("Hola mundo")
+      body: ListView(
+        children:  [
+        ...options.map(
+          (e) => ListTile(
+            title: Text(e),
+            trailing: const Icon(Icons.safety_divider),
+          )
+          ).toList()
       ],)
     );
   }
